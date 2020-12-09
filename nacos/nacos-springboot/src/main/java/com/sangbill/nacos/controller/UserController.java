9a687cfc68096881ad2541d88a94a63cda97ec70
734156776@qq.com <734156776@qq.com>
2020-12-09 22:10:49 +0800

    添加nacos

---
+++ nacos/nacos-springboot/src/main/java/com/sangbill/nacos/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.sangbill.nacos.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+class UserController {
+
+    @Value("${user.name}")
+    private String userName;
+
+    @Value("${user.age}")
+    private int age;
+
+	@RequestMapping("/user")
+	public String getUser() {
+		return userName+" "+age;
+	}
+}
